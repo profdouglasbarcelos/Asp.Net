1f46502da4c79558e70e7005855c3981a94ff98f
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-05-15 11:24:42 -0300

    Criando formulario de projeto

---
+++ Aula1505/Aula1505/CadastroProjeto.aspx.cs
@@ -0,0 +1,39 @@
+﻿using Aula1505.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Aula1505
+{
+    public partial class CadastroProjeto : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCancelar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+        }
+
+        private void LimparCampos()
+        {
+            txtNome.Text = string.Empty;
+            chkAtivo.Checked = false;
+        }
+
+        protected void btnSalvar_Click(object sender, EventArgs e)
+        {
+            Projeto prj = new Projeto();
+            prj.Nome = txtNome.Text;
+            prj.Descricao = txtDescricao.Text;
+            prj.Ativo = chkAtivo.Checked;
+
+
+        }
+    }
+}
---
+++ Aula1505/Aula1505/Models/Projeto.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace Aula1505.Models
+{
+    public class Projeto
+    {
+        [Key]
+        public int ProjetoID { get; set; }
+
+        [Required,StringLength(20)]
+        public string Nome { get; set; }
+
+        [Display(Name ="Descrição"), DataType(DataType.MultilineText)]
+        public string Descricao { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
