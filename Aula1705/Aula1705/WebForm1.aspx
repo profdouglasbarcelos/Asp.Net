b13784d658277b9af61923e6bec296091a87759c
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-05-17 10:06:50 -0300

    Uso de ViewState e Session;

---
+++ Aula1705/Aula1705/WebForm1.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Aula1705.WebForm1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+        <ContentTemplate>
+            <asp:Label ID="lblQuantidadeCliques" runat="server" Text=""></asp:Label>
+            <asp:Button ID="btnSalvar" runat="server" Text="Salvar" OnClick="btnSalvar_Click" />
+            <asp:Button ID="btnEnviarCliques" runat="server" Text="Enviar Cliques" OnClick="btnEnviarCliques_Click" />
+        </ContentTemplate>
+    </asp:UpdatePanel>
+</asp:Content>
---
+++ Aula1705/Aula1705/WebForm1.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Aula1705
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        public int QuantidadeCliques
+        {
+            get
+            {
+                return Convert.ToInt32(ViewState["qtdCliques"]);
+            }
+
+            set
+            {
+                ViewState["qtdCliques"] = value;
+            }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+                QuantidadeCliques = 0;
+        }
+
+        protected void btnSalvar_Click(object sender, EventArgs e)
+        {
+            //QuantidadeCliques = Convert.ToInt32(ViewState["qtdCliques"]);
+
+            QuantidadeCliques++;
+            lblQuantidadeCliques.Text = QuantidadeCliques.ToString();
+            //ViewState["qtdCliques"] = QuantidadeCliques;
+        }
+
+        protected void btnEnviarCliques_Click(object sender, EventArgs e)
+        {
+            Session.Add("QtdCliquesSession", QuantidadeCliques);
+            Response.Redirect("~/WebForm2.aspx");
+        }
+    }
+}
