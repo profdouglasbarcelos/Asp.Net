13f3880c702506cc89831f4a6bcc97a244d207cc
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-06-01 19:52:07 -0300

    Adicionando RowCommand a grid de categorias para começar o processo de edição e exclusão

--- Aula2505_EF_MF/Aula2505_EF_MF/Views/Categorias/Lista.aspx
+++ Aula2505_EF_MF/Aula2505_EF_MF/Views/Categorias/Lista.aspx
@@ -1,5 +1,11 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Lista.aspx.cs" Inherits="Aula2505_EF_MF.Views.Categorias.Lista" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
     <h1>Categorias</h1>
-    <asp:GridView ID="gvCategorias" runat="server"></asp:GridView>
+    <asp:GridView ID="gvCategorias" runat="server" OnRowCommand="gvCategorias_RowCommand">
+        <Columns>
+            <asp:ButtonField CommandName="Excluir" Text="Excluir" />
+            <asp:ButtonField CommandName="Editar" Text="Editar" />
+        </Columns>
+
+    </asp:GridView>
 </asp:Content>
--- Aula2505_EF_MF/Aula2505_EF_MF/Views/Categorias/Lista.aspx.cs
+++ Aula2505_EF_MF/Aula2505_EF_MF/Views/Categorias/Lista.aspx.cs
@@ -17,7 +17,6 @@ namespace Aula2505_EF_MF.Views.Categorias
             gvCategorias.DataBind();
 
 
-
             // ---- Gerando Alert JS
 
             // sem updatepanel
@@ -27,6 +26,36 @@ namespace Aula2505_EF_MF.Views.Categorias
             // com update panel
             //var ID2 = "Whatever the data is";
             //ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('" + ID2 + "');", true);
+        }
+
+        protected void gvCategorias_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            // Detectando a linha que foi clicada
+            int linha = Convert.ToInt32(e.CommandArgument);
+
+            // Recuperando o id do Objeto (lembrando que 2, do Cells[2], é a coluna onde esta o id)
+            int idObjeto = Convert.ToInt32(gvCategorias.Rows[linha].Cells[2].Text);
+
+            // Nome do comando para saber a acao
+            string command = e.CommandName;
+
+            // Proximos passos
+
+            // adicionar id na Session
+
+            if (command.Equals("Excluir"))
+            {
+                // Redirecionando para tela de exclusao
+                Response.Redirect("Excluir.aspx");
+            }
+            else
+            {
+                if(command.Equals("Editar"))
+                {
+                    // Redirecionando para tela de edicao
+                    Response.Redirect("Editar.aspx");
+                }
+            }
         }
     }
 }
