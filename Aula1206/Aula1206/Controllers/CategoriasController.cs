705210d172f1accf17b2054c370db637631ccca0
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-06-12 10:52:04 -0300

    View Create; Model Categoria;

--- Aula1206/Aula1206/Controllers/CategoriasController.cs
+++ Aula1206/Aula1206/Controllers/CategoriasController.cs
@@ -1,7 +1,5 @@
-﻿using System;
+﻿using Aula1206.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Aula1206.Controllers
@@ -11,17 +9,45 @@ namespace Aula1206.Controllers
         // GET: Categorias
         public ActionResult Index()
         {
-            List<string> categorias = new List<string>();
+            //List<string> categorias = new List<string>();
 
-            categorias.Add("Carros");
-            categorias.Add("Motos");
-            categorias.Add("Barcos");
-            categorias.Add("Aviões");
-            categorias.Add("Caminhões");
+            //categorias.Add("Carros");
+            //categorias.Add("Motos");
+            //categorias.Add("Barcos");
+            //categorias.Add("Aviões");
+            //categorias.Add("Caminhões");
+
+            List<Categoria> categorias = new List<Categoria>();
+
+            categorias.Add(
+                    new Categoria()
+                    {
+                        Nome = "Carros",
+                        Descricao = "Super carros de coleção",
+                        Ativo = true
+                    }
+                );
+            categorias.Add(new Categoria() { Nome = "Motos", Descricao = "As motos mais velozes do mundo" });
+            categorias.Add(new Categoria() { Nome = "Barcos", Descricao = "Flutuantes" });
+            categorias.Add(new Categoria() { Nome = "Aviões", Descricao = "Supersônicos" });
+            categorias.Add(new Categoria() { Nome = "Caminhões" });
 
             ViewBag.ListaCategorias = categorias;
 
+            return View(categorias);
+        }
+
+        // GET
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Categoria categoria)
+        {
+
+            return View(categoria);
         }
     }
 }
