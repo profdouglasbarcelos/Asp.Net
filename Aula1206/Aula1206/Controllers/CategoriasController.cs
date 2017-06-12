aa55f4a75aea3a9c4e9d8381f851b3cc8cd08068
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-06-12 09:25:32 -0300

    Criando Controller e View de categoria (usando List<string> e ViewBag);

---
+++ Aula1206/Aula1206/Controllers/CategoriasController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aula1206.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            List<string> categorias = new List<string>();
+
+            categorias.Add("Carros");
+            categorias.Add("Motos");
+            categorias.Add("Barcos");
+            categorias.Add("Aviões");
+            categorias.Add("Caminhões");
+
+            ViewBag.ListaCategorias = categorias;
+
+            return View();
+        }
+    }
+}
