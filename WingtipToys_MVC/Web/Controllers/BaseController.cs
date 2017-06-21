d6f345e0db6cb47782cdd64cd749afee342183a3
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-06-21 11:20:16 -0300

    Ajustes no controle de acesso

---
+++ WingtipToys_MVC/Web/Controllers/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult GetCategorias()
+        {
+            var categorias = db.Categorias.ToList();
+            return View();
+        }
+    }
+}
