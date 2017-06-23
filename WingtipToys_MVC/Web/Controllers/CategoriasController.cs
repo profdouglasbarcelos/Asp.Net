a236d47a8452793ee4aea68b032c7be53b6eca35
profDouglasBarcelos <prof.douglasbarcelos@gmail.com>
2017-06-22 21:41:14 -0300

    Finalizando a controller de Categorias e adicionando views

--- WingtipToys_MVC/Web/Controllers/CategoriasController.cs
+++ WingtipToys_MVC/Web/Controllers/CategoriasController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Web.Mvc;
 using Web.Models;
 using System.Linq;
+using BaseModels;
+using System.Net;
 
 namespace Web.Controllers
 {
@@ -15,5 +17,134 @@ namespace Web.Controllers
             var categorias = db.Categorias.ToList();
             return View(categorias);
         }
+
+        //  --------- Cadastrar nova Categoria ------------
+        //GET
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Categoria categoria)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Categorias.Add(categoria);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(categoria);
+        }
+
+
+        // --------- Detalhes --------
+        // GET
+        public ActionResult Details(int? id)
+        {
+            // Nao passou id?
+            if(id == null /*id.HasValue*/)
+            {
+                // ERRO HTTP 400
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Categoria categoria = db.Categorias.Find(id);
+
+            // nao encontrou o objeto com este id
+            if(categoria == null)
+            {
+                // ERRO HTTP 404
+                return HttpNotFound();
+            }
+
+            // caso a categoria seja encontrada, retorna ela para a view
+            return View(categoria);
+        }
+
+
+        // ------------ Editar -------------
+        // GET
+        public ActionResult Edit(int? id)
+        {
+            // Nao passou id?
+            if (id == null /*id.HasValue*/)
+            {
+                // ERRO HTTP 400
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Categoria categoria = db.Categorias.Find(id);
+
+            // nao encontrou o objeto com este id
+            if (categoria == null)
+            {
+                // ERRO HTTP 404
+                return HttpNotFound();
+            }
+
+            // caso a categoria seja encontrada, retorna ela para a view
+            return View(categoria);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Categoria categoria)
+        {
+            if(ModelState.IsValid)
+            {
+                db.Entry(categoria).State = 
+                    System.Data.Entity.EntityState.Modified;
+
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(categoria);
+        }
+
+        // ------------- Excluir ------------
+
+
+        // GET
+        public ActionResult Delete(int? id)
+        {
+            // Nao passou id?
+            if (id == null /*id.HasValue*/)
+            {
+                // ERRO HTTP 400
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Categoria categoria = db.Categorias.Find(id);
+
+            // nao encontrou o objeto com este id
+            if (categoria == null)
+            {
+                // ERRO HTTP 404
+                return HttpNotFound();
+            }
+
+            // caso a categoria seja encontrada, retorna ela para a view
+            return View(categoria);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Categoria categoria = db.Categorias.Find(id);
+
+            db.Categorias.Remove(categoria);
+
+            //db.Entry(categoria).State = 
+            //    System.Data.Entity.EntityState.Deleted;
+
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
     }
 }
