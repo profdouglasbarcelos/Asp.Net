10166767f819212c228b1321a6443def43ab841d
profdouglasbarcelos <prof.douglasbarcelos@gmail.com>
2018-05-15 20:04:37 -0300

    Criando projeto Aula1505_MVC1 (adicionando CategoriasController e Categorias/Index.cshtml)

---
+++ Aulas/Aula1505_MVC1/Controllers/CategoriasController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Aula1505_MVC1.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            List<string> categorias = new List<string>();
+
+            categorias.Add("Estudo");
+            categorias.Add("Lazer");
+            categorias.Add("Refeição");
+            categorias.Add("Trabalho");
+
+            ViewBag.MinhasCategorias = categorias;
+
+            return View();
+        }
+    }
+}
---
+++ Aulas/Aula1505_MVC1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Aula1505_MVC1.MvcApplication" Language="C#" %>
