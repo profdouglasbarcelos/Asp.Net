527ed86c59fe6e9ecdb77678004a7486358bcdf7
profdouglasbarcelos <prof.douglasbarcelos@gmail.com>
2018-05-15 19:19:32 -0300

    Criando contexto e models a partir do banco (Database First)

---
+++ Aulas/Aula1005_Modelos/ClientesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1005_Modelos
+{
+    class ClientesController
+    {
+        private MeuBancoDados contexto = new MeuBancoDados();
+
+        public void SalvarCliente(Clientes cli)
+        {
+            contexto.Clientes.Add(cli);
+            contexto.SaveChanges();
+        }
+    }
+}
