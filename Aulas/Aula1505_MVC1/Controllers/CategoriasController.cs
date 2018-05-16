e4d059cebd17b760707770fd7f58979ccc4ca0d4
profdouglasbarcelos <prof.douglasbarcelos@gmail.com>
2018-05-15 21:28:21 -0300

    Adicionando CategoriasController

---
+++ Aulas/Aula1505_MVC1/Controllers/CategoriasController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Aula1505_MVC1.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
