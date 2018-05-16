758c122e759c96fc662bc71c4b4bacd115a8caed
profdouglasbarcelos <prof.douglasbarcelos@gmail.com>
2018-05-15 21:24:35 -0300

    Adicionando model Categoria

---
+++ Aulas/Aula1505_MVC1/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿namespace Aula1505_MVC1.Models
+{
+    public class Categoria
+    {
+        public int CategoriaID { get; set; }
+
+        public string Nome { get; set; }
+
+        public bool Ativo { get; set; }
+
+    }
+}
