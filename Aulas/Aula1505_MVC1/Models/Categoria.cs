01eff67295c3cc3e943850fd811a5ae5ce99bfa2
profdouglasbarcelos <prof.douglasbarcelos@gmail.com>
2018-05-22 19:57:23 -0300

    Adicionando DataAnnotations

--- Aulas/Aula1505_MVC1/Models/Categoria.cs
+++ Aulas/Aula1505_MVC1/Models/Categoria.cs
@@ -1,9 +1,12 @@
-﻿namespace Aula1505_MVC1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aula1505_MVC1.Models
 {
     public class Categoria
     {
         public int CategoriaID { get; set; }
 
+        [Display(Name ="Título")]
         public string Nome { get; set; }
 
         public bool Ativo { get; set; }
