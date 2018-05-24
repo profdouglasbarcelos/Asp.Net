7578d958af07367761a928d57693a4fd072b724e
profdouglasbarcelos <prof.douglasbarcelos@gmail.com>
2018-05-24 19:57:53 -0300

    Criando Dbset na classe MeuContexto para as classes Usuario, Categoria e Atividade

--- Aulas/Aula2205_Entity/Models/DAL/MeuContexto.cs
+++ Aulas/Aula2205_Entity/Models/DAL/MeuContexto.cs
@@ -17,6 +17,9 @@ namespace Aula2205_Entity.Models.DAL
         }
 
 
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Atividade> Atividades { get; set; }
 
     }
 }
