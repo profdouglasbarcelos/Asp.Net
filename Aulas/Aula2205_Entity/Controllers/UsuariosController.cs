ea282dd502cb31686612f11224d35094e9b10a92
profdouglasbarcelos <prof.douglasbarcelos@gmail.com>
2018-05-24 20:22:50 -0300

    Criando ActionResult Index e View Index para classe Usuario

---
+++ Aulas/Aula2205_Entity/Controllers/UsuariosController.cs
@@ -0,0 +1,23 @@
+﻿using Aula2205_Entity.Models;
+using Aula2205_Entity.Models.DAL;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Aula2205_Entity.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        // GET: Usuarios
+        public ActionResult Index()
+        {
+            MeuContexto contexto = new MeuContexto();
+            List<Usuario> usuarios = contexto.Usuarios.ToList();
+
+            return View(usuarios);
+        }
+
+
+
+    }
+}
