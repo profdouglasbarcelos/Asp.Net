e002203a9c309eda9d12b77f506b95037df42b26
profdouglasbarcelos <prof.douglasbarcelos@gmail.com>
2018-05-24 21:56:06 -0300

    Criando action e view "Edit" para classe Usuario

--- Aulas/Aula2205_Entity/Controllers/UsuariosController.cs
+++ Aulas/Aula2205_Entity/Controllers/UsuariosController.cs
@@ -39,6 +39,7 @@ namespace Aula2205_Entity.Controllers
             return View(usuario);
         }
 
+        //GET
         public ActionResult Details(int? id)
         {
             if(id == null)
@@ -53,6 +54,44 @@ namespace Aula2205_Entity.Controllers
             if(usu == null)
             {
                 return HttpNotFound();
+            }
+
+            return View(usu);
+        }
+
+        //GET
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            MeuContexto contexto = new MeuContexto();
+
+            Usuario usu = contexto.Usuarios.Find(id);
+
+            if (usu == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(usu);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Usuario usu)
+        {
+            if(ModelState.IsValid)
+            {
+                MeuContexto contexto = new MeuContexto();
+
+                contexto.Entry(usu).State = 
+                    System.Data.Entity.EntityState.Modified;
+
+                contexto.SaveChanges();
+                return RedirectToAction("Index");
             }
 
             return View(usu);
