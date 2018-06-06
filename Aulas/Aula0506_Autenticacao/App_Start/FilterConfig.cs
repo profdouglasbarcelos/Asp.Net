7daa82c925b5d50f2d70c26d2fbb055644e139f3
profdouglasbarcelos <prof.douglasbarcelos@gmail.com>
2018-06-05 22:18:48 -0300

    Criando projeto Aula0506_Autenticacao

---
+++ Aulas/Aula0506_Autenticacao/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Aula0506_Autenticacao
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
